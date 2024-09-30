15822fa0dbec2960bacf947dd0435573ea80dbf7
Welly Shen <hivoid19@gmail.com>
2024-09-30 19:53:31 +0800

    Setup basic view script

--- src/new-block/main/view.ts
+++ src/new-block/main/view.ts
@@ -1,7 +1,15 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
 import { fetchProfile } from '../utils';
 
 document.addEventListener( 'DOMContentLoaded', () => {
-	const test = document.querySelector( '.gravatar-block' );
-	const attrs = JSON.parse( test.dataset.attrs );
-	console.log( 'LOG ===> ', attrs );
+	const gravatarBlocks = document.querySelectorAll( '.gravatar-block' );
+
+	gravatarBlocks.forEach( ( block ) => ( block.innerHTML = __( 'Loading…', 'gravatar-enhanced' ) ) );
 } );
