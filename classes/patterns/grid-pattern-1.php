fc34f6f313f39f437f615d336c90a1c0bf951bf4
Welly Shen <welly.shen@automattic.com>
2025-02-09 05:13:07 +0800

    Update pattern content

--- classes/patterns/grid-pattern-1.php
+++ classes/patterns/grid-pattern-1.php
@@ -1,23 +1,23 @@
 <!-- wp:group {"className":"gravatar-grid-pattern-1","layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group gravatar-grid-pattern-1"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<div class="wp-block-group gravatar-grid-pattern-1"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Role","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
 <!-- /wp:group -->
