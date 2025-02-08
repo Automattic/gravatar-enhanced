422b5f0e95356a2e210ec621f2cf25db9c883cd4
Welly Shen <welly.shen@automattic.com>
2025-02-09 03:35:39 +0800

    Update pattern content

--- classes/patterns/grid-pattern-1.php
+++ classes/patterns/grid-pattern-1.php
@@ -1,29 +1,29 @@
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<div class="wp-block-group"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<div class="wp-block-group"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<div class="wp-block-group"><!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /-->
 
-<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":80,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
+<!-- wp:gravatar/block {"layout":"portrait","avatarUrlSizeParam":334,"demoProfile":{"display_name":"Display Name","job_title":"Job Title","location":"City, Country"},"userType":"email","deletedElements":{"description":true,"verifiedAccounts":true}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
