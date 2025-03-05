8eb81ab23f56fe698420911c2a296c471b19a4d9
John <johng75@gmail.com>
2025-03-05 12:55:34 +0000

    Link to the gravatar.com profile URL (#78)

--- src/comments/index.ts
+++ src/comments/index.ts
@@ -38,14 +38,14 @@ async function fetchUserProfile( email ) {
 function suggestProfile( profile ) {
 	const author = document.getElementById( 'author' ) as HTMLInputElement;
 	const url = document.getElementById( 'url' ) as HTMLInputElement;
-	const firstVerified = profile.verified_accounts.length > 0 ? profile.verified_accounts[ 0 ] : null;
+	const profileUrl = profile.profile_url;
 
 	if ( author && author.value === '' ) {
 		author.value = profile.display_name;
 	}
 
-	if ( url && url.value === '' && firstVerified ) {
-		url.value = firstVerified.url;
+	if ( url && url.value === '' && profileUrl ) {
+		url.value = profileUrl;
 	}
 }
 
