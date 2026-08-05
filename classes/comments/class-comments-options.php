a43ec5ae81957f1676da47ac4331de5b01a04259
Faisal Ahammad <faisalahammad24@gmail.com>
2026-08-05 17:08:30 +0600

    feat: add Fediverse hovercard support
    Adds hovercard support for Fediverse profile URLs in comments.
    
    New REST endpoint: /gravatar-enhanced/v1/fediverse/lookup
    Resolves Fediverse profiles via WebFinger + ActivityPub actor JSON.
    Renders a lightweight custom hovercard on comment avatar hover.
    Toggleable in Discussion settings (enabled by default).
    
    Fixes #97

--- classes/comments/class-comments-options.php
+++ classes/comments/class-comments-options.php
@@ -5,17 +5,23 @@ namespace Automattic\Gravatar\GravatarEnhanced\Comments;
 /**
  * @phpstan-type CommentsOptionsArray = array{
  *   enabled: bool,
+ *   fediverse_hovercards: bool,
  * }
  */
 class Options {
 	/** @var bool */
 	public $enabled;
 
+	/** @var bool */
+	public $fediverse_hovercards;
+
 	/**
 	 * @param bool $enabled
+	 * @param bool $fediverse_hovercards
 	 */
-	public function __construct( $enabled ) {
-		$this->enabled = $enabled;
+	public function __construct( $enabled, $fediverse_hovercards ) {
+		$this->enabled             = $enabled;
+		$this->fediverse_hovercards = $fediverse_hovercards;
 	}
 
 	/**
@@ -23,7 +29,8 @@ class Options {
 	 */
 	public function to_array() {
 		return [
-			'enabled' => $this->enabled,
+			'enabled'              => $this->enabled,
+			'fediverse_hovercards' => $this->fediverse_hovercards,
 		];
 	}
 
@@ -32,6 +39,9 @@ class Options {
 	 * @return Options
 	 */
 	public static function from_array( $options ) {
-		return new Options( $options['enabled'] );
+		return new Options(
+			$options['enabled'],
+			$options['fediverse_hovercards']
+		);
 	}
 }
